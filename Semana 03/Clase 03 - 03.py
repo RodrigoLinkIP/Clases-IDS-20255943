290121d384ffbe11958e48035284822cb7b2a4d3
RodrigoLinkIP <104653543+RodrigoLinkIP@users.noreply.github.com>
2025-09-27 23:02:02 -0600

    Ejemplos de variables

--- Semana 03/Clase 03 - 03.py
+++ Semana 03/Clase 03 - 03.py
@@ -1,2 +1,20 @@
-variable = 123
+year = 2025
+temperature = 28.5
+
+pi = 3.14159
+
+nota = 5.5
+aprobacion = 5
+
+validacion = nota != 5.5
+
+estado = nota > aprobacion
+
+grad = year + 5
+
+print(grad)
+print(temperature)
+print(pi)
+
+print(validacion)
 
