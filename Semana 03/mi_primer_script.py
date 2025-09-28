4d4aa8fddfc83a44621f18e812984b0558f7129e
RodrigoLinkIP <104653543+RodrigoLinkIP@users.noreply.github.com>
2025-09-27 22:55:07 -0600

    Creación inicial de archivos

---
+++ Semana 03/mi_primer_script.py
@@ -0,0 +1,7 @@
+variable1 = 100
+variable2 = 150
+variable3 = variable1 + variable2
+# Suma de variables
+""" Este es un ejemplo de
+comentario multili... bla bla bla, hagamos caso omiso va"""
+print(variable3)
