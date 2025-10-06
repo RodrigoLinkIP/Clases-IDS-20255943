ad05cedba2057705b77f73fe2928ebda8d71064d
RodrigoLinkIP <104653543+RodrigoLinkIP@users.noreply.github.com>
2025-10-05 22:34:16 -0600

    Clase 3 grabada semana 4

---
+++ Semana 04/Clase 04 - 03.py
@@ -0,0 +1,19 @@
+
+my_string = "ABCDEFCCC" # string, cadena de caracteres
+
+"""letra = int(input("Indique el indice de la letra mostrar (del 0 al 4): "))
+print(my_string[letra])"""
+
+#print(len(my_string))
+#print(my_string[1:5:50]), para subsetting por slices
+
+#rint("O" not in my_string)
+
+#print("AJP"*8)
+
+#print(min(my_string))
+#print(max(my_string))
+
+#print(my_string.index("A"))
+
+print(my_string.count("C"))
