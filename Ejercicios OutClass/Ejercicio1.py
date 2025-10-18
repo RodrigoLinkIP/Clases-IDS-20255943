fa7b7535bcaa892b89b4f3ebaecd3e6e2be8e398
RodrigoLinkIP <104653543+RodrigoLinkIP@users.noreply.github.com>
2025-10-18 08:41:17 -0600

    Update Ejercicio1.py

--- Ejercicios OutClass/Ejercicio1.py
+++ Ejercicios OutClass/Ejercicio1.py
@@ -44,7 +44,7 @@ for i in range(0, n):
     sp.append(int(input()))
 print(sp.index(min(sp)) + 1)"""
 
-
+r = ""
 minis = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
 mayus = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
 r.rfind(".")
